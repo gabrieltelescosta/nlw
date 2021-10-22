03b84b4ae3f094f9d0416bac9b69ff284ab2049d
Gabriel Teles <gabrieltelesc1@gmail.com>
2021-10-22 09:45:20 -0300

    include api github

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const linksSocialMedia = {
-    github: "gabrieltelescr",
+    github: "gabrieltelescosta",
     youtube: "gabrieltelescr",
     facebook: "gabrieltelescr",
     twitter: "gabrieltelescr",
@@ -14,8 +14,23 @@ function changeSocialMediaLinks() {
     }
 }
 
-function changeGithubLink() {
-    userGit.href = `https://github.com/${linksSocialMedia.github}`
-}
-changeGithubLink()
+
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name,
+                userBio.textContent = data.bio,
+                userGit.href = data.html_url,
+                userGitName.textContent = data.login,
+                userPhoto.src = data.avatar_url
+        })
+
+
+
+}
+getGitHubProfileInfos()
